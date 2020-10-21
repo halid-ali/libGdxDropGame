5a73a9ad8292883c834dec02e054c466fc1409b6
Halid Ali <halid.ali@methodpark.de>
2020-10-21 20:31:34 +0200

    version-2 test commit

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -42,15 +42,15 @@ public class Drop extends ApplicationAdapter {
 
     @Override
     public void create() {
-        //load the images for the droplet and the bucket, 64x64 pixels each
+        //load the images for the droplet and the bucket, 64x64 pixels each.
         dropImage = new Texture(Gdx.files.internal("drop.png"));
         bucketImage = new Texture(Gdx.files.internal("bucket.png"));
 
-        //load the drop and sound effect and the rain background music
+        //load the drop and sound effect and the rain background music.
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
         rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
-        //start the playback of the background music immediately
+        //start the playback of the background music immediately.
         rainMusic.setLooping(true);
         rainMusic.play();
 
